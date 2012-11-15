78fd5e032c7a1f8869ad827d8f2c5134329a15a1
Friedrich Lindenberg <friedrich@pudo.org>
2012-11-15 18:53:30 +0000

    Processing scripts for the we version

---
+++ book-config.js
@@ -0,0 +1,19 @@
+paginationConfig = {
+    'sectionStartMarker': '.objavi-subsection',
+    'sectionTitleMarker': '.objavi-subsection-heading',
+    'chapterStartMarker': 'h1',
+    'chapterTitleMarker': 'h1',
+    'flowElement': "document.getElementById('book')",
+    'alwaysEven': true,
+    'columns': 1,
+    'enableFrontmatter': true,
+    'bulkPagesToAdd': 50, 
+    'pagesToAddIncrementRatio': 1.4, 
+    'frontmatterContents': '<div id="booktitle">Spending Data Handbook</div>'
+	+ '<div id="booksubtitle"></div><div id="bookeditors">'
+	+ 'Iteration 1</div><div class="pagebreak">'
+	+ '</div><div id="copyrightpage">Copyright 2012: The Contributors<br>License: CC-BY</div>'
+	+ '<div class="pagebreak"></div>',
+    'autoStart': true,
+}
+
---
+++ update.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#curl -o book.zip http://okfn.booktype.pro/export/spending-data-handbook/export
+unzip -o -d book book.zip
+
+mkdir -p static
+rm static/*
+cp book/static/* static/
+
+mogrify -geometry '600x600>' static/*
+
+
